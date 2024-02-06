2757eb876abf98ca99120ea31378f2ef3d33d3ac
Mzwa1080 <mshawnlouw@gmail.com>
2024-02-06 13:59:36 +0200

    it overrides the first content

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ let fs = require('fs')
 
 // console.log(fs);
 
-fs.writeFile('data.txt', 'shwan content!', function (err) {
+fs.writeFile('data.txt', 'Mzwamadoda Louw first line of code', function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
+
+fs.writeFile('data.txt', 'Im adding a second line required by Joel', (err)=>{
+    console.log(err);
+})
 
   fs.appendFile('data.txt', '\n This is my second data ', (err)=> {
     if(err) throw err;
